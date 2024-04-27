df623448859bb65150247de03e21e26b3a20d059
NeptuneRjo <rjo9000@outlook.com>
2024-04-27 15:21:36 -0400

    Add db initialization for pgsql

--- BugTracker.API/Program.cs
+++ BugTracker.API/Program.cs
@@ -1,5 +1,6 @@
 using BugTracker.BLL;
 using BugTracker.DAL;
+using BugTracker.DAL.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -9,12 +10,22 @@ services.RegisterDALDependencies(builder.Configuration);
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+// https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-8.0&tabs=visual-studio
+using (var scope = app.Services.CreateScope())
+{
+    var providerServices = scope.ServiceProvider;
+
+    var context = providerServices.GetRequiredService<DataContext>();
+    context.Database.EnsureCreated();
 }
 
 //app.UseHttpsRedirection();
