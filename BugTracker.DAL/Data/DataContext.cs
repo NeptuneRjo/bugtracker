205b0762a09f076f71c2a27cc04195c9640a47d5
NeptuneRjo <rjo9000@outlook.com>
2024-04-29 16:32:27 -0400

    Add account table and define relationships

--- BugTracker.DAL/Data/DataContext.cs
+++ BugTracker.DAL/Data/DataContext.cs
@@ -12,12 +12,14 @@ namespace BugTracker.DAL.Data
         public DbSet<Issue> Issues { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Project> Projects { get; set; }
+        public DbSet<Account> Accounts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Issue>().ToTable("Issue");
             modelBuilder.Entity<Comment>().ToTable("Comment");
             modelBuilder.Entity<Project>().ToTable("Project");
+            modelBuilder.Entity<Account>().ToTable("Account");
 
             modelBuilder.Entity<Project>()
                 .HasMany(e => e.Issues)
@@ -30,6 +32,16 @@ namespace BugTracker.DAL.Data
                 .WithOne(e => e.Issue)
                 .HasForeignKey(e => e.IssueId)
                 .OnDelete(DeleteBehavior.Cascade);
+        
+            modelBuilder.Entity<Account>()
+                .HasMany(e => e.CreatedProjects)
+                .WithOne(e => e.Creator)
+                .HasForeignKey(e => e.CreatorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Account>()
+                .HasMany(e => e.CollaborationProjects)
+                .WithMany(e => e.Collaborators);
         }
     }
 }
