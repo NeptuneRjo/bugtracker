71eeca69195ff0d05bd99601e0b345ed4e20dba0
NeptuneRjo <rjo9000@outlook.com>
2024-04-29 16:33:52 -0400

    Update project model

--- BugTracker.DAL/Entities/Project.cs
+++ BugTracker.DAL/Entities/Project.cs
@@ -1,23 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BugTracker.DAL.Entities
 {
+    public enum ProjectStatus
+    {
+        Active,
+        Abandoned,
+        Closed
+    }
+
+    [Table("Projects")]
     public class Project
     {
-        public int Id { get; set; }
+        public int ProjectId { get; set; }
         public string Title { get; set; }
         public ICollection<Issue> Issues { get; set; }
         public DateTime CreatedAt { get; set; }
-        public string? Poster { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public List<string> Labels { get; set; }
+        public ProjectStatus Status { get; set; }
+        
+        public Account Creator { get; set; }
+        public int CreatorId { get; set; }
 
+        public ICollection<Account> Collaborators { get; set; }
+        
         public Project()
         {
             Issues = new List<Issue>();
             CreatedAt = DateTime.UtcNow;
+            Collaborators = new List<Account>();
+
+            // Set the default labels
+            Labels = new List<string>()
+            {
+                "Bug",
+                "Feature",
+                "Documentation",
+                "Needs Investigation",
+                "Question",
+                "Help Wanted"
+            };
+
+            Status = ProjectStatus.Active;
         }
     }
 }
